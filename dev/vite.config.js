28f32a967800dd8272c3f511a4a7bdfbd1b5e83b
slarmoo <matthew.s.medford@gmail.com>
2025-06-25 02:35:52 -0400

    updated distribution

---
+++ dev/vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    plugins: [],
+    build: {
+        outDir: '../', // <- set your custom folder here
+    },
+})
